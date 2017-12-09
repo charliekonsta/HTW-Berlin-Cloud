09a8806b264038d5cdae05195a7b8704b0a947c0
charliekonsta <34392930+charliekonsta@users.noreply.github.com>
2017-12-09 02:03:36 -0800

    Update 03b-BasicAuth.js

--- 03b-BasicAuth.js
+++ 03b-BasicAuth.js
@@ -1,43 +1,30 @@
 var http = require('http');
-
 var url = require('url');
-
 var crypto = require("crypto");
-
 var port = process.env.PORT || 8081;
 
 
 
 http.createServer(function (request, response) {
 
-   // Send the HTTP header 
-
-   // HTTP Status: 200 : OK
-
-   // Content Type: text/plain
+ // Send the HTTP header 
+ // HTTP Status: 200 : OK
+ // Content Type: text/plain
 
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
-   
 
     var queryData = url.parse(request.url, true).query;
-
     var firstname =  queryData.fn;
-
     var lastname =  queryData.ln;
-
     var requestId = crypto.randomBytes(16).toString("hex");
-
-
-
+   
     var headers = request.headers;
-
     var authorization = headers['authorization'];
 
    
 
     var username = null;
-
     var password = null;
 
 
@@ -47,17 +34,12 @@ http.createServer(function (request, response) {
     {
 
         var tmp = authorization.split(' ');     // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
-
         var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
-
         var plain_auth = buf.toString();        // read it back out as a string At this point plain_auth = "username:password"
 
 
-
         var creds = plain_auth.split(':');      // split on a ':'
-
         username = creds[0];
-
         password = creds[1];
 
     }
@@ -85,28 +67,23 @@ http.createServer(function (request, response) {
                 
 
                 if (authorization) 
-
                     responseMessage += '\nYour provided Authorization header is: '+authorization;
 
                 
 
                 if (username)
-
                     responseMessage += '\nYour provided username is: '+username;
 
 
 
                 if (password)
-
                     responseMessage += '\nYour provided password is: '+password;
 
                 
 
                 responseMessage += '\n\nRequest Id: '+requestId;
-
                 response.end(responseMessage);
 
-       
 
             });       
 
@@ -127,5 +104,4 @@ http.createServer(function (request, response) {
 
 
 // Console will print the message
-
 console.log("Server running at http://localhost:%d", port);
